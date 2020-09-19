1c1f6be3e08321f1a3bd69a5cd69b27d1c297899
Eldiyar <eldiiardev@gmail.com>
2020-09-19 18:58:18 +0600

    set avatart2

--- src/components/set-avatar/set-avatar.js
+++ src/components/set-avatar/set-avatar.js
@@ -6,21 +6,21 @@ import withLogin from "../provider/login/with-login";
 
 class SetAvatar extends Component {
     state = {
-        image: '',
+        avatar: '',
         errors:{}
     }
     onChangeFile = (e) => {
         const elem = e.target
         this.setState( {
-                 image: elem.files[0]
+            avatar: elem.files[0]
         })
     }
     onSubmit = (e) => {
         e.preventDefault()
-        this.props.setAvatar({image: this.state.image}).then((data) => {
+        this.props.setAvatar({avatar: this.state.avatar}).then((data) => {
             notice('Сохранено', 'success')
             console.log(data)
-            this.setState({image: null})
+            this.setState({avatar: null})
         }).catch(async ({res}) => {
             if (res && res.status === 400) {
                 const errors = await res.json()
@@ -43,8 +43,8 @@ class SetAvatar extends Component {
                 <div className="form-group">
                     <input onChange={this.onChangeFile} name="image"
                            type="file"/>
-                    {state.errors.image && <div className="text-danger">
-                        {state.errors.image[0]}
+                    {state.errors.avatar && <div className="text-danger">
+                        {state.errors.avatar[0]}
                     </div>}
 
                 </div>
